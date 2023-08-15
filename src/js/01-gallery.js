719f1fd9ec73ce35a720a9afdbf8e69eb714d547
IKovalov27 <121258263+IKovalov27@users.noreply.github.com>
2023-08-15 17:51:07 +0200

    272727

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,24 +5,28 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
-const galleryMarkup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-  </a>
-</li>`;
-  })
-  .join('');
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryEl.innerHTML = galleryMarkup;
+function createGalleryItemsMarkup(items) {
+    return galleryItems
+    .map(({ preview, original, description}) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__item" href="${original}" rel='noreferrer noopener nofollow'>
+                    <img class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    />
+                </a>
+            </div> 
+        `;
+    })
+    .join('');
+};
 
-const lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-  captions: true,
-  captionsData: 'alt',
-  captionSelector: 'img',
-  captionPosition: 'bottom',
-  captionDelay: 250,
+let lightboxGallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
 });
