60dae06fbee14d07ac50b4c0810325d2e7733179
IKovalov27 <121258263+IKovalov27@users.noreply.github.com>
2023-08-18 21:08:06 +0000

    Deploying to gh-pages from @ IKovalov27/goit-js-hw-08@4e4fc73870a7e690083ce44f0a51cecfe926734d 🚀

--- 02-video.ad5e8e5f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Player(e);t.on("timeupdate",throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3));const r=localStorage.getItem("videoplayer-current-time");r&&t.setCurrentTime(r);
-//# sourceMappingURL=02-video.ad5e8e5f.js.map
